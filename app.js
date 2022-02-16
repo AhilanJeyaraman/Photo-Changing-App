7cf9979db66e90bb78d810fd0f1cbd3e2ef02955
Ahilan <ahilan.jeyaraman@gmail.com>
2022-02-16 17:47:37 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//Select 7 colors
+let colors = [ 'red', 'blue', 'green', 'orange', 'purple', 'black', 'yellow', 'pink'];
+
+let imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = 'img/AJ-0076.jpg';
+
+imgArray[1] = new Image();
+imgArray[1].src = 'img/AJ-0078.jpg';
+
+imgArray[2] = new Image();
+imgArray[2].src = 'img/AJ-0083.jpg';
+
+imgArray[3] = new Image();
+imgArray[3].src = 'img/AJ-0084.jpg';
+
+imgArray[4] = new Image();
+imgArray[4].src = 'img/AJ-0087.jpg';
+
+//change the background of canvas when button is clicked
+let button = document.getElementById('button1');
+
+button.addEventListener('click', function(){
+    //select a random number between 0 - 7
+    //let index = parseInt((Math.random()*colors.length)+1);
+    let index = parseInt((Math.random()*imgArray.length)+1);
+    //grab the canvas
+    let canvas = document.getElementById('canvas');
+
+    //canvas.style.background = `${colors[index]}`
+    canvas.style.backgroundImage = `url(${imgArray[index].src})`;
+})
